db4d2c4235eb2f413c6fa7800bf9d69dcdd40e8e
Francis Ibe <49306271+Cidospark@users.noreply.github.com>
2023-09-23 11:56:52 +0100

    updated

--- DevsTutorialCenterMVC/Migrations/20230922100240_FirstMigration.Designer.cs
+++ DevsTutorialCenterMVC/Migrations/20230922100240_FirstMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DevsTutorialCenterMVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DevsTutorialCenterMVC.Migrations
 {
     [DbContext(typeof(DevsTutorialCenterMVCContext))]
-    partial class DevsTutorialCenterMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20230922100240_FirstMigration")]
+    partial class FirstMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
